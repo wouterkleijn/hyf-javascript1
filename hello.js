a9cfea80196ed143f4661351790b7d61792e9d18
Wouter <wouterkleijn@gmail.com>
2018-04-15 15:45:43 +0200

    first Javascript test

---
+++ hello.js
@@ -0,0 +1,17 @@
+
+const months = ['January', 'February', 'December'];
+//                 ^0       ^1          ^2
+
+const currentMonth = 1;
+
+let hyf = null;
+
+const hello = 'Hello world!';
+
+const part = hello.slice(0, 7)
+const upper = hello.toUpperCase();
+const startsWithHello = hello.startsWith('Hello');
+
+console.log(part[0], upper, 'startsWithHello');
+
+
